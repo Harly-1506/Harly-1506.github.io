1e764120561723be42d8c7a607c5b17ff9c58f10
Harly-1506 <tranminhhai1506@gmail.com>
2023-08-20 01:18:49 +0700

    [230820] Setting_configs

--- setting.js
+++ setting.js
@@ -1,12 +1,14 @@
+
+let focus = document.getElementById('focus').value;
+let breakTime = document.getElementById('break').value;
+let reps = document.getElementById('reps').value;
+let color = document.getElementById('color').value;
+let timerFont = document.getElementById('timerFont').value;
+let sessionFont = document.getElementById('sessionFont').value;
+let alignment = document.getElementById('alignment').value;
+let link = `https://harly-1506.github.io/timer.html?focus=${focus}&break=${breakTime}&reps=${reps}&color=${encodeURIComponent(color)}&timerFont=${encodeURIComponent(timerFont)}&sessionFont=${encodeURIComponent(sessionFont)}&alignment=${alignment}`;
 function updateLinkAndPreview() {
-    let focus = document.getElementById('focus').value;
-    let breakTime = document.getElementById('break').value;
-    let reps = document.getElementById('reps').value;
-    let color = document.getElementById('color').value;
-    let timerFont = document.getElementById('timerFont').value;
-    let sessionFont = document.getElementById('sessionFont').value;
-    let alignment = document.getElementById('alignment').value;
-    let link = `https://harly-1506.github.io/timer.html?focus=${focus}&break=${breakTime}&reps=${reps}&color=${encodeURIComponent(color)}&timerFont=${encodeURIComponent(timerFont)}&sessionFont=${encodeURIComponent(sessionFont)}&alignment=${alignment}`;
+    
     document.getElementById('link').href = link;
     document.getElementById('link').innerHTML = link;
 
