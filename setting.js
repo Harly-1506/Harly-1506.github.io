b52d08aea3a15a4f106977e27a4903dfa7d69f15
Harly-1506 <tranminhhai1506@gmail.com>
2023-08-20 15:59:13 +0700

    [230820] Add more functions

--- setting.js
+++ setting.js
@@ -3,10 +3,11 @@ function updateLinkAndPreview() {
     let breakTime = document.getElementById('break').value;
     let reps = document.getElementById('reps').value;
     let color = document.getElementById('color').value;
+    let pomodoro = document.getElementById("PomodoroTimer").value;
     let timerFont = document.getElementById('timerFont').value;
     let sessionFont = document.getElementById('sessionFont').value;
     let alignment = document.getElementById('alignment').value;
-    let link = `https://harly-1506.github.io/timer.html?focus=${focus}&break=${breakTime}&reps=${reps}&color=${encodeURIComponent(color)}&timerFont=${encodeURIComponent(timerFont)}&sessionFont=${encodeURIComponent(sessionFont)}&alignment=${alignment}`;
+    let link = `https://harly-1506.github.io/${pomodoro}.html?focus=${focus}&break=${breakTime}&reps=${reps}&color=${encodeURIComponent(color)}&timerFont=${encodeURIComponent(timerFont)}&sessionFont=${encodeURIComponent(sessionFont)}&alignment=${alignment}`;
     document.getElementById('link').href = link;
     document.getElementById('link').innerHTML = link;
 
